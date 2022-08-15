5ecbeb2f4ce706e4ebe772f0b32895f0f1d5511e
tbor00 <tbor00>
2022-08-15 02:05:02 -0300

    initial styles of navbar

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -1,60 +1,71 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
-import { Button, Tooltip } from '../index'
+import { Button, Tooltip, Text } from '../index'
 
 export const Navbar = () => {
+    const uuid = React.useId()
     const router = useRouter()
 
     type navLinksType = {
-        [key: string]: string | boolean
+        name: string
+        description: string
+        href: string
     }
+
     const navLinks: navLinksType[] = [
         {
-            name: 'Trade',
-            description: 'Description',
-            direction: 'left',
-            href: 'string',
-            showTooltip: false
+            name: 'Home',
+            description: 'Go to the main page',
+            href: '/'
         },
         {
-            name: 'Earn',
-            description: 'Description',
-            direction: 'left',
-            href: 'string',
-            showTooltip: false
+            name: 'About me',
+            description: 'A little about me to get to know each other better',
+            href: '/me'
         },
         {
-            name: 'Win',
-            description: 'Description',
-            direction: 'left',
-            href: 'string',
-            showTooltip: false
+            name: 'Projects',
+            description: 'Projects in which I participate',
+            href: '/projects'
         },
         {
-            name: 'NFT',
-            description: 'Description',
-            direction: 'left',
-            href: 'string',
-            showTooltip: false
+            name: 'Contributions',
+            description: 'My contributions to the community',
+            href: '/contributions'
         }
     ]
 
     return (
-        <nav style={{ backgroundColor: 'rgb(39, 38, 44)' }} className=" h-12 flex items-center justify-between">
-            <div className="mx-20">
-                <div className="flex items-center gap-8">
-                    {navLinks.map((link, index) => (
-                        <Fragment key={index}>
-                            <Tooltip isOnHover tooltipMessage={link.description as string}>
-                                <Button className="text-purple-200">
-                                    <span>{link.name}</span>
-                                </Button>
-                            </Tooltip>
-                        </Fragment>
-                    ))}
-                </div>
+        <nav style={{ backgroundColor: 'rgb(39, 38, 44)' }} className="shadow-md h-14 flex items-center justify-center">
+            <div className="hidden md:flex items-center justify-center gap-6">
+                {navLinks.map((link, index) => (
+                    <React.Fragment key={`link-${index}-${uuid}`}>
+                        <Tooltip isOnHover tooltipMessage={link.description}>
+                            <Button
+                                variant="buttonLink"
+                                className={link.href === router.pathname ? 'text-purple-300 font-bold' : ''}
+                                onClick={() => router.push(link.href)}
+                            >
+                                <span>{link.name}</span>
+                            </Button>
+                        </Tooltip>
+                    </React.Fragment>
+                ))}
             </div>
         </nav>
+    )
+}
+
+export const Footer = () => {
+    return (
+        <footer className="flex items-center justify-center my-4">
+            <Text variant="small" className="flex gap-2">
+                <span> Made with 💜 and</span>
+                <a href="https://nextjs.org/" target="__blank" className="underline text-purple-300">
+                    Nextjs
+                </a>
+            </Text>
+        </footer>
     )
 }
 
@@ -63,6 +74,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
         <>
             <Navbar />
             {children}
+            <Footer />
         </>
     )
 }

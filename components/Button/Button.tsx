b72e05ddd7855079b52da7f689b8d09608d7ddcb
tbor00 <tbor00>
2022-08-15 02:03:12 -0300

    add more styles and variants at Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: 'primary' | 'secondary' | 'outline'
+    variant?: 'primary' | 'secondary' | 'outline' | 'buttonLink'
     size?: 'xl' | 'lg' | 'md' | 'sm'
     label?: string
     classNameLabel?: string
     styleLabel?: React.CSSProperties
-    ref?: any
 }
 
 export const Button = ({ variant, size, className, style, label, classNameLabel, styleLabel, children, ...props }: ButtonProps) => {
     const defineClassName: () => string = React.useCallback(() => {
         const colorVariants: { [key: string]: string } = {
-            primary: '',
+            primary: 'shadow-lg bg-purple-500 text-white rounded-xl hover:bg-purple-700 py-3 px-3 transition-all hover:-translate-y-0.5',
             secondary: '',
-            outline: ''
+            buttonLink: 'text-purple-200/75 rounded-xl hover:bg-purple-300/5 hover:text-white py-3 px-3 text-sm',
+            outline: 'shadow-lg outline rounded-xl py-3 px-3 hover:outline-purple-400 hover:-translate-y-0.5 transition-transform'
         }
 
         const sizeVariants: { [key: string]: string } = {
